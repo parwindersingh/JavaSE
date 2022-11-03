095ff765599eb0ec210e001ab8599b91c32c5753
Parwinder34570 <pranota4570@conestogac.on.ca>
2022-11-03 18:00:53 -0400

    LeetCode Two Sum

--- src/leetcode/TwoSum.java
+++ src/leetcode/TwoSum.java
@@ -1,20 +1,18 @@
+//Leetcode: https://leetcode.com/problems/two-sum/
 package leetcode;
-
+import java.util.HashMap;
+import java.util.Map;
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
-        int result[] = new int[2];
-        boolean flag = true;
-        for(int i=0; i<nums.length && flag; i++){
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[i]+nums[j] == target){
-                    result[0] = i;
-                    result[1] = j;
-                    flag = false;
-                    break;
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++) {
+            int difference = target - nums[i];
+            if( map.containsKey(difference) ) {
+                return new int[] { map.get(difference) , i };
             }
+            map.put( nums[i], i );
         }
-        return result;
+        return null;
     }
     public static void main(String args[]){
         int result[] = twoSum(new int[]{3,2,3},6);
