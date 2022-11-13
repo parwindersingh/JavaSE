3be8d8a7efd933665f5ab6f0ff82ec4c16e88d62
Parwinder34570 <parwindersingh780@gmail.com>
2022-11-13 10:43:22 -0500

    Maximum Number Of Words Found In Sentences

---
+++ src/leetcode/array/MaximumNumberOfWordsFoundInSentences.java
@@ -0,0 +1,22 @@
+/*
+https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+Input: sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+Output: 6
+ */
+package leetcode.array;
+
+import java.util.Arrays;
+
+public class MaximumNumberOfWordsFoundInSentences {
+    public static int mostWordsFound(String[] sentences) {
+        int max=0;
+        for (int i = 0; i < sentences.length; i++) {
+            max = Math.max(max,sentences[i].split(" ").length);
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int ans = mostWordsFound(new String[]{"alice and bob love leetcode", "i think so too", "this is great thanks very much"});
+        System.out.println(ans);
+    }
+}
